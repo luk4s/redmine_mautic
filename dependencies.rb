b8370c0efe22f9f2a9dbe49d70468fb6a34fa23b
Lukáš Pokorný <luk4s.pokorny@gmail.com>
2018-04-28 20:48:35 +0200

    move to ryspec

--- dependencies.rb
+++ dependencies.rb
@@ -1,12 +1,10 @@
 # Dependencies which are loaded in main application or in dummy application
 # All gems must be in "rys" group
 
-git_source(:easy_git){ |name| "git@git.easy.cz:platform-2.0/#{name}.git" }
-
 group :rys do
   group :default do
-    gem 'rys', easy_git: 'rys', branch: 'master'
-    gem 'easy_core', easy_git: 'easy_core', branch: 'master'
+    gem 'rys', github: 'easysoftware/rys', branch: 'master'
+    gem 'ryspec', github: 'easysoftware/ryspec', branch: 'master'
   end
 
   group :development do
@@ -19,6 +17,6 @@ group :rys do
     gem 'poltergeist'
     gem 'database_cleaner', '~> 1.6.1'
     gem 'capybara'
-    gem "selenium-webdriver"
+#    gem "selenium-webdriver"
   end
 end
--- lib/tasks/redmine_mautic.rake
+++ lib/tasks/redmine_mautic.rake
@@ -1,14 +1,16 @@
-if !Rails.env.production? && defined?(EasyCore) && defined?(RSpec)
+begin
   require 'rspec/core/rake_task'
 
   namespace :redmine_mautic do
     desc 'Runs rspec tests'
     RSpec::Core::RakeTask.new(:spec) do |task, task_args|
       args = []
-      args << '--require' << EasyCore::Engine.root.join('spec/spec_helper')
+      args << '--require' << Ryspec::Engine.root.join('spec/spec_helper')
 
       task.rspec_opts = args
       task.pattern = RedmineMautic::Engine.root.join('spec/**/*_spec.rb')
     end
   end
+rescue StandardError
+
 end
